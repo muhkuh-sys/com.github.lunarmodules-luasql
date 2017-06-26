fd05b96cc24e88ae1e306f6e26bdc5340957d223
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-06-26 14:47:27 +0200

    Add the Ubuntu version to the archive name.

--- .lxc_build_ubuntu_1704_x86.sh
+++ .lxc_build_ubuntu_1704_x86.sh
@@ -31,9 +31,9 @@ lxc exec ${CONTAINER} -- bash -c 'apt-get install --assume-yes libncurses5-dev l
 
 # Build the 32bit version.
 lxc exec ${CONTAINER} -- bash -c 'cd /tmp/work && bash .build03_linux.sh'
-lxc exec ${CONTAINER} -- bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1604_x86_lua5.1.tar.gz --gzip --directory /tmp/work/build/linux/lua5.1/luasql/install .'
-lxc exec ${CONTAINER} -- bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1604_x86_lua5.2.tar.gz --gzip --directory /tmp/work/build/linux/lua5.2/luasql/install .'
-lxc exec ${CONTAINER} -- bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1604_x86_lua5.3.tar.gz --gzip --directory /tmp/work/build/linux/lua5.3/luasql/install .'
+lxc exec ${CONTAINER} -- bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1704_x86_lua5.1.tar.gz --gzip --directory /tmp/work/build/linux/lua5.1/luasql/install .'
+lxc exec ${CONTAINER} -- bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1704_x86_lua5.2.tar.gz --gzip --directory /tmp/work/build/linux/lua5.2/luasql/install .'
+lxc exec ${CONTAINER} -- bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1704_x86_lua5.3.tar.gz --gzip --directory /tmp/work/build/linux/lua5.3/luasql/install .'
 lxc file pull ${CONTAINER}/tmp/work/build/build_ubuntu_1704_x86_lua5.1.tar.gz build/
 lxc file pull ${CONTAINER}/tmp/work/build/build_ubuntu_1704_x86_lua5.2.tar.gz build/
 lxc file pull ${CONTAINER}/tmp/work/build/build_ubuntu_1704_x86_lua5.3.tar.gz build/
