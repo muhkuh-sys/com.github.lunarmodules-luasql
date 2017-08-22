4d6e608892f9e00e9370d4917162c530af08192c
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-08-22 11:47:40 +0200

    Return an error if the requested platform is not supported by the package.

--- luasql/installer/lua5.1/install.lua
+++ luasql/installer/lua5.1/install.lua
@@ -1,13 +1,16 @@
 local t = ...
 local strDistId, strDistVersion, strCpuArch = t:get_platform()
+local tResult
 
 if strDistId=='windows' then
   if strCpuArch=='x86' then
     t:install('windows_x86/lua_plugins/luasql/mysql.dll',            '${install_lua_cpath}/luasql/')
     t:install('windows_x86/lua_plugins/luasql/sqlite3.dll',          '${install_lua_cpath}/luasql/')
+    tResult = true
   elseif strCpuArch=='x86_64' then
     t:install('windows_x86_64/lua_plugins/luasql/mysql.dll',         '${install_lua_cpath}/luasql/')
     t:install('windows_x86_64/lua_plugins/luasql/sqlite3.dll',       '${install_lua_cpath}/luasql/')
+    tResult = true
   end
 
 elseif strDistId=='ubuntu' then
@@ -15,29 +18,35 @@ elseif strDistId=='ubuntu' then
     if strCpuArch=='x86' then
       t:install('ubuntu_1204_x86/lua_plugins/luasql/mysql.so',       '${install_lua_cpath}/luasql/')
       t:install('ubuntu_1204_x86/lua_plugins/luasql/sqlite3.so',     '${install_lua_cpath}/luasql/')
+      tResult = true
     elseif strCpuArch=='x86_64' then
       t:install('ubuntu_1204_x86_64/lua_plugins/luasql/mysql.so',    '${install_lua_cpath}/luasql/')
       t:install('ubuntu_1204_x86_64/lua_plugins/luasql/sqlite3.so',  '${install_lua_cpath}/luasql/')
+      tResult = true
     end
 
   elseif strDistVersion=='14.04' then
     if strCpuArch=='x86' then
       t:install('ubuntu_1404_x86/lua_plugins/luasql/mysql.so',       '${install_lua_cpath}/luasql/')
       t:install('ubuntu_1404_x86/lua_plugins/luasql/sqlite3.so',     '${install_lua_cpath}/luasql/')
+      tResult = true
     elseif strCpuArch=='x86_64' then
       t:install('ubuntu_1404_x86_64/lua_plugins/luasql/mysql.so',    '${install_lua_cpath}/luasql/')
       t:install('ubuntu_1404_x86_64/lua_plugins/luasql/sqlite3.so',  '${install_lua_cpath}/luasql/')
+      tResult = true
     end
 
   elseif strDistVersion=='16.04' then
     if strCpuArch=='x86' then
       t:install('ubuntu_1604_x86/lua_plugins/luasql/mysql.so',       '${install_lua_cpath}/luasql/')
       t:install('ubuntu_1604_x86/lua_plugins/luasql/sqlite3.so',     '${install_lua_cpath}/luasql/')
+      tResult = true
     elseif strCpuArch=='x86_64' then
       t:install('ubuntu_1604_x86_64/lua_plugins/luasql/mysql.so',    '${install_lua_cpath}/luasql/')
       t:install('ubuntu_1604_x86_64/lua_plugins/luasql/sqlite3.so',  '${install_lua_cpath}/luasql/')
+      tResult = true
     end
   end
 end
 
-return true
+return tResult
